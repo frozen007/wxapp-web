eca52c2e6e7c7d05b17d4b3afd159f8b27d54a16
zhaomingyu <zhaomingyu@meituan.com>
2022-07-27 20:37:05 +0800

    split steps

--- docker-run.sh
+++ docker-run.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 # 定义应用组名
-group_name='wxapp-web'
+group_name=$1
 # 定义应用名称
-app_name='wxapp-web-drone'
-if [ -z "$1" ]; then
+app_name=$2
+# 定义应用版本
+app_version=$3
+instance_id=$4
+if [ -z "$instance_id" ]; then
   app_instance_name=${app_name}_01
 else
-  app_instance_name=${app_name}_$1
+  app_instance_name=${app_name}_${instance_id}
 fi
-# 定义应用版本
-app_version='1.0-SNAPSHOT'
 # 定义应用环境
 profile_active='dev'
 echo '----copy jar----'
